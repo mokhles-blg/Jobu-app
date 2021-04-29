89974c34cf207417f05cabab74c62199dd523a8e
[Baligh <[lassouedba@gmail.com]>
2021-04-29 01:47:25 +0100

    small fixes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const connectDB = require("./config/connectDB");
 const fileUpload = require("express-fileupload");
+const path = require("path");
 
 require("dotenv").config();
 // instance app
@@ -20,6 +21,15 @@ app.use("/api/getSavedJobs", require("./router/getSavedJobs"));
 app.use("/api/post", require("./router/post"));
 app.use("/api/application", require("./router/application"));
 
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 // PORT
 const PORT = process.env.PORT;
 // create server
